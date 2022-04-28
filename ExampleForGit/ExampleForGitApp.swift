dd1b26949197c2720ea07a2c432fc54d93809c63
Anna Loshakova <ann.losh99@mail.ru>
2022-04-28 15:47:22 +0300

    Initial Commit

---
+++ ExampleForGit/ExampleForGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleForGitApp.swift
+//  ExampleForGit
+//
+//  Created by Анна Лошакова on 28.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExampleForGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
